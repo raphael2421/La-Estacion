d859ca783cd03661bcd5640b98aa26d84094cc48
jean Belmon <jean@jeanbelmont.com>
2021-03-18 11:17:13 -0600

    proyecto terminado

--- public/js/proyecto.js
+++ public/js/proyecto.js
@@ -21,6 +21,7 @@ const chevron_left = document.querySelector('.chevron_left');
 const chevron_right = document.querySelector('.chevron_right');
 const slides_container = document.querySelector('.slides_container');
 const slide = document.querySelector('.slide');
+const close_popup = document.querySelectorAll('.close_popup');
 // const slide = document.querySelector('.slide');
 
 // IDEA:
@@ -30,8 +31,8 @@ slides_container.setAttribute('style', `grid-template-columns: repeat(${slidesCo
 slidesContent.forEach((i, ï, ä) => {
    slides_container.innerHTML += `
       <div class="slide">
-         <img src="../media/slider-proyecto/${window.innerWidth >= 600 ? i.large : i.small}" alt="" class="modelos_img">
-          <div class="modelos_title">
+         <img src="../media/slider-proyecto/${window.innerWidth >= 600 ? i.large : i.small}" alt="" class="modelos_img ${i.title}">
+          <div class="modelos_title ${i.title}">
                   ${i.title}
             </div>
          <div class="modelos_tipo">
@@ -56,6 +57,8 @@ function go_left() {
    } else {
       slides_container.style.transform = `translate(-${window.innerWidth * slideN}px)`;
    }
+   // chevron_left.classList.add('display_none');
+   // chevron_right.classList.remove('display_none');
 
 } // go_left
 
@@ -72,30 +75,52 @@ function go_right() {
    } else {
       slides_container.style.transform = `translate(-${window.innerWidth * slideN}px)`;
    }
+   // chevron_right.classList.add('display_none');
+   // chevron_left.classList.remove('display_none');
 }
 
 /********************************************/
-let initialCoords = 0;
-let finalCoords = 0;
+// let initialCoords = 0;
+// let finalCoords = 0;
 
-slides_container.addEventListener('touchstart', function (e) {
-   initialCoords = e.touches[0].clientX;
-   // console.log(initialCoords);
-});
+// slides_container.addEventListener('touchstart', function (e) {
+//    initialCoords = e.touches[0].clientX;
+//    // console.log(initialCoords);
+// });
 
-slides_container.addEventListener('touchmove', function (e) {
-   finalCoords = e.touches[0].clientX;
-   // console.log(finalCoords);
-});
+// slides_container.addEventListener('touchmove', function (e) {
+//    finalCoords = e.touches[0].clientX;
+//    // console.log(finalCoords);
+// });
 
-slides_container.addEventListener('touchend', function (e) {
-   if (finalCoords < initialCoords - 50) {
-      go_right();
-   }
-   else if (finalCoords > initialCoords + 50) {
-      go_left();
-   }
-   else {
+// slides_container.addEventListener('touchend', function (e) {
+//    if (finalCoords < (initialCoords - 70) ) {
+//       go_right();
+//    }
+//    else if (finalCoords > (initialCoords + 70) ) {
+//       go_left();
+//    }
+// });
+
+
+/*********************************/
+
+document.addEventListener('click', function(e){
+   if (e.target.classList.contains('allende')) {
+      e.stopPropagation();
       e.preventDefault();
+      document.getElementById('allende').classList.remove('display_none');
    }
+   if (e.target.classList.contains('morelos')) {
+      e.stopPropagation();
+      e.preventDefault();
+      document.getElementById('morelos').classList.remove('display_none');
+   }
+});
+
+close_popup.forEach((i, ï, ä)=>{
+   i.addEventListener('click', ()=>{
+      document.getElementById('morelos').classList.add('display_none');
+      document.getElementById('allende').classList.add('display_none');
+   });
 });
